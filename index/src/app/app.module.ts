d9e405303177c127ef1056de613b62edc5b603f8
TMrex-dev <123335008+TMrex-dev@users.noreply.github.com>
2023-11-22 17:26:59 -0500

    WE MADE A NAVBAR BOIS
    LETS GOOOO
    (check for bugs)

---
+++ index/src/app/app.module.ts
@@ -0,0 +1,35 @@
+// Import necessary modules and components from Angular
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { HeaderModule } from './header/header.module';
+
+// Decorator to mark this class as an NgModule
+@NgModule({
+  // Declarations: Components, directives, and pipes belonging to this module
+  declarations: [
+    HeaderComponent
+
+    // Other components, directives, pipes...
+  ],
+  // Imports: Other modules whose exported classes are needed by the components in this module
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+
+
+    // Other imported modules...
+  ],
+  // Providers: Services available to this module and its components
+  providers: [
+    // Services...
+  ],
+  // Bootstrap: The main application view, called the root component, that hosts all other app views
+  bootstrap: []
+})
+// Exporting the class AppModule as an NgModule
+export class AppModule { }
