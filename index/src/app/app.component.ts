66c1fbc76c41110e9048f753e9fc61d9b2ac316b
TMrex-dev <123335008+TMrex-dev@users.noreply.github.com>
2023-11-25 00:28:20 -0500

    Routing is Active
    you can now navigate to different pages

--- index/src/app/app.component.ts
+++ index/src/app/app.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { HeaderModule } from './header/header.module';
+import { RouterOutlet, RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderModule],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, RouterModule, HomeComponent],
+  template: `
+    <main>
+      <header>
+      </header>
+      <nav>
+    <ul>
+      <li><a routerLink="/home">Home</a></li>
+      <li><a routerLink="/about">About</a></li>
+      <li><a routerLink="/login">Login</a></li>
+    <!-- Add more navigation links here -->
+    </ul>
+    </nav>
+      <section>
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'index';
+  title = 'homes';
 }
--- index/src/app/app.routes.ts
+++ index/src/app/app.routes.ts
@@ -1,19 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+const routeConfig: Routes = [
+  { path: '', title: 'Home Page', component: HomeComponent}, // Default route redirects to Home
+  { path: 'home', title: 'HomePage', component: HomeComponent},
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
-  // Add other routes as needed
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export default routeConfig; 
+ 
