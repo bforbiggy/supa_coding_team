e7a6deca0963ea93de59857c35f933f763efc0e2
TMrex-dev <123335008+TMrex-dev@users.noreply.github.com>
2023-11-25 20:44:06 -0500

    made routing work again

--- index/src/app/app.component.ts
+++ index/src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule} from '@angular/router';
 import { HeaderModule } from './header/header.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderModule],
-  templateUrl: './app.component.html',
+  imports: [RouterModule, RouterOutlet, HeaderModule],
+  template: `
+    <main>
+      <header>
+      </header>
+      <nav>
+    <ul>
+      <li><a routerLink="/home">Home</a></li>
+      <li><a routerLink="/about">About</a></li>
+      <li><a routerLink="/login">Login</a></li>
+    <!-- Add more navigation links here -->
+    </ul>
+    </nav>
+      <section>
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
