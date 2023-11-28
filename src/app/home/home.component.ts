640d54e0ca6a28c6885a0ee288ff0e2447ba1fdf
Infirmax <77449439+Infirmax@users.noreply.github.com>
2023-11-27 23:38:07 -0500

    load data from back end
    starting backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,9 +6,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+  
 export class HomeComponent {
+  //we are taking reponse from steam using the api
+  steamResponse: string = 'No response yet';
 
-  constructor() { }
+  constructor() { 
+    this.loadReviews();
+  }
+
+  async loadReviews() {
+    const apiUrl = 'https://localhost:3000/';
+    const response = await fetch(apiUrl);
+    const data = await response.text();
+    this.steamResponse = data;
+  }
 
   ngOnInit(): void {
   }
--- src/app/steamapi.js
+++
@@ -1,4 +0,0 @@
-const SteamAPI = require('steamapi');
-const steam = new SteamAPI('steam token');
-steam.resolve('https://steamcommunity.com/id/DimGG').then(id => {
-	console.log(id)});
