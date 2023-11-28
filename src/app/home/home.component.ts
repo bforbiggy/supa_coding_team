39bdca195e6fbb59183f535387f6faa74c248472
Infirmax <77449439+Infirmax@users.noreply.github.com>
2023-11-28 01:18:16 -0500

    reusable/dynamic code
    added parameter to loadgame, so this way we can figure out how to determine what app id we can put in it in the future. Removed variable reviewUrl, no point in having it

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,29 +12,27 @@ export class HomeComponent {
   name: String = "";
   
   constructor() { 
-    this.loadGame();
+    this.loadGame(413150);
   }
 
-  async loadGame() {
+  async loadGame(app_id: Number) {
     // App headers
     const requestHeaders: HeadersInit = new Headers();
-    requestHeaders.set('app_id', '413150');
+    requestHeaders.set('app_id', `${app_id}`);
 
     // Load game reviews
-    const reviewUrl = 'http://localhost:3000/steamreviews';
-    const reviewResponse = await fetch(reviewUrl, {
+    const reviewResponse = await fetch('http://localhost:3000/steamreviews', {
       headers: requestHeaders
     });
     const reviewData = await reviewResponse.json();
     this.reviewCount = reviewData.query_summary.total_reviews;
 
     // Load game name
-    const nameUrl = 'http://localhost:3000/steamname';
-    const nameResponse = await fetch(nameUrl, {
+    const nameResponse = await fetch('http://localhost:3000/steamname', {
       headers: requestHeaders
     });
     const nameData = await nameResponse.json();
-    this.name = nameData["413150"].data.name;
+    this.name = nameData[`${app_id}`].data.name;
   }
 
   ngOnInit(): void {
